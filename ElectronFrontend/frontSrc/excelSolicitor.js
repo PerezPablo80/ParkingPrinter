372dca2645f080c0ac7e66bb2dc7e39e22822e21
perezpablo80 <perez.pablo@gmail.com>
2024-07-27 17:50:11 -0300

    Req 2 Save excel file and ui/ux 1

--- ElectronFrontend/frontSrc/excelSolicitor.js
+++ ElectronFrontend/frontSrc/excelSolicitor.js
@@ -1,13 +1,37 @@
 var helperData=[];
-document.getElementById("save").addEventListener("click", async (datos) => {
-	const data = [
-		{ Name: "John", Age: 30, City: "New York" },
-		{ Name: "Jane", Age: 25, City: "San Francisco" },
-		{ Name: "Peter", Age: 35, City: "Chicago" },
-	];
-	await window.electron.saveExcel(data);
+document.getElementById("addVehicle").addEventListener("click", async(datos) => {
+	// console.log('DAtos:',datos);
+	let input=document.getElementById('vehiclePlate').value;
+	let time=document.getElementById('timeSpan').textContent;
+	let date=document.getElementById('dateSpan').textContent;
+	console.log('Input:',input);
+	console.log('time:',time);
+	document.getElementById('vehiclePlate').value="";
+	let aux={Id:maxId(),Vehicle:input,'Start time':time,'Start date':date};
+	console.log("newVehicle:",aux)
+	addVehicle(aux);
+	await saveFile(helperData);
+	loadFile();
+	// window.electron.addVehicle(datos);
+	// await window.electron.saveExcel(data);
 });
+document.getElementById('save-data').addEventListener("click",async ()=>{
+await saveFile(helperData);
+});
+async function saveFile(data){
+	console.log('DATA:',data);
 
+	const res = await window.electron.saveExcel(data);
+	console.log('RES:',res);
+}
+function maxId(){
+	let max=0;
+	helperData.forEach((h)=>{
+		if(h['Id']>max)max=h['Id'];
+	});
+	console.log('MAX+1:',max+1);
+	return max+1;
+}
 /*
 document.getElementById("load").addEventListener("click", async () => {
 	await loadFile();
@@ -17,14 +41,29 @@ async function loadFile(){
 		const data = await window.electron.loadExcel("./assets/archivo.xlsx");
 	const tbody = document.getElementById("data-table").querySelector("tbody");
 	tbody.innerHTML = "";
-	// console.log('DATA:',data);
+	helperData=[];
 	let displayKeys=['Vehicle','Start time'];
+	let maxId=0;
 	data.forEach((row, index) => {
 		// console.log('Row:',row)
 		if(!row.hasOwnProperty('End time')){
 		const tr = document.createElement("tr");
-		let d = [];
+		// let d = [];
 		let added=false;
+
+		// Add a button with an action based on the row's ID
+		const buttonTd = document.createElement("td");
+		const button = document.createElement("button");
+		button.textContent = "Finalizar";
+		button.className="btn btn-success";
+		button.onclick = () => {
+			// Define the action based on row ID
+			console.log(`Button clicked for row with ID: ${row.Id}`);
+			// Perform the action here
+		};
+		buttonTd.appendChild(button);
+		tr.appendChild(buttonTd);
+
 		// Append only specified values to the tr
 		displayKeys.forEach((key) => {
 			if (row.hasOwnProperty(key)) {
@@ -39,30 +78,26 @@ async function loadFile(){
 			td.textContent='No';
 			tr.appendChild(td);
 		}
-		// Add a button with an action based on the row's ID
-		const buttonTd = document.createElement("td");
-		const button = document.createElement("button");
-		button.textContent = "Finalizar";
-		button.className="btn btn-success";
-		button.onclick = () => {
-			// Define the action based on row ID
-			console.log(`Button clicked for row with ID: ${row.Id}`);
-			// Perform the action here
-		};
-		buttonTd.appendChild(button);
-		tr.appendChild(buttonTd);
-		helperData.push(d);
-		tbody.appendChild(tr);
-		d.push(row);	
-	}		
+		if(row.Id!=0)
+			tbody.appendChild(tr);
+		// d.push(row);	
+	}	
+	// if(row.Id>maxId){
+	// 	maxId=row.Id;
+	// }
+	helperData.push(row);	
 	});
+	// helperData.push({maxId:maxId});	
 	console.log('HelperData:',helperData);
 	}catch(e){
 		console.log("Exception loading file::",e);
 	}
 }
-function addCar(data){
+function addVehicle(data){
+	
+	console.log('HELPER pre:',helperData);
 	helperData.push(data);
+	console.log('HELPER post:',helperData);
 }
 function modifyCar(data){
 	console.log('helperData:',helperData);
--- ElectronFrontend/main.js
+++ ElectronFrontend/main.js
@@ -7,8 +7,8 @@ let mainWindow;
 
 function createWindow() {
 	mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
+		width: 1280,
+		height: 1024,
 		webPreferences: {
 			preload: path.join(__dirname, "preload.js"),
 			contextIsolation: true,
@@ -33,6 +33,134 @@ app.on("activate", () => {
 	}
 });
 
+ipcMain.on("print", (event, content) => {
+	// Read the CSS styles
+	// const styles = fs.readFileSync(path.join(__dirname, "printableStyles.css"), "utf-8");
+
+	// Sanitize the styles to ensure they don't contain any characters that might break the HTML structure
+	// const sanitizedStyles = styles.replace(/<\/style>/g, "<\\/style>").replace(/<!--/g, "<\\!--");
+	const sanitizedContent = content.replace(/<\/style>/g, "<\\/style>").replace(/<!--/g, "<\\!--");
+	const printCSS = `
+    <style>
+      @page {
+        size: 80mm auto;
+        margin: 0;
+      }
+      body {
+        width: 80mm;
+        font-size: 10px;
+        padding: 3mm;
+      }
+	 #barcode{
+	 	width:70mm;
+	}
+		.spanclass {
+		font-size: 14px;
+		padding:2mm;
+		}
+      /* Add more specific styles here */
+    </style>
+  `;
+
+	// Save the HTML content to a temporary file
+	// const tempFilePath = path.join(__dirname, "temp_print.html");
+	// fs.writeFileSync(tempFilePath, htmlContent);
+
+	const printWindow = new BrowserWindow({ show: false });
+	// printWindow.loadFile(tempFilePath);
+	printWindow.webContents.loadURL(
+		`data:text/html,${encodeURIComponent(`
+	<!DOCTYPE html>
+	<html>
+	  <head>
+		${printCSS}
+	  </head>
+	  <body>
+		${content}
+	  </body>
+	</html>
+	`)}`
+	);
+	printWindow.webContents.on("did-finish-load", () => {
+		printWindow.webContents.print({ silent: true, printBackground: true }, (success, errorType) => {
+			if (!success) {
+				console.log("Print failed: ", errorType);
+			} else {
+				console.log("Print success");
+			}
+			printWindow.close();
+			// fs.unlinkSync(tempFilePath); // Delete the temporary file after printing
+		});
+	});
+	printWindow.webContents.on("did-fail-load", (event, errorCode, errorDescription) => {
+		console.log("Failed to load content: ", errorDescription);
+		printWindow.close();
+		fs.unlinkSync(tempFilePath); // Delete the temporary file in case of failure
+	});
+});
+
+ipcMain.handle("save-excel", async (event, data) => {
+	let filePath = "./assets/archivo.xlsx";
+	try {
+		const workbook = new ExcelJS.Workbook();
+		// Check if the file exists and read it
+		if (fs.existsSync(filePath)) {
+			await workbook.xlsx.readFile(filePath);
+		} else {
+			workbook.addWorksheet("Hoja 1");
+		}
+
+		const worksheet = workbook.getWorksheet("Hoja 1");
+
+		// Define columns if they are not already defined
+		if (!worksheet.columns.length) {
+			worksheet.columns = Object.keys(data[0]).map((key) => ({ header: key, key }));
+		}
+
+		// Update rows based on IDs
+		data.forEach((newRow) => {
+			const row = worksheet.getRow(newRow.Id + 1); // Assuming Id is 1-based and matches the row number
+			//if (row.getCell("A").value === newRow.Id)
+			const existingRow = worksheet.findRow(newRow.Id);
+			if (existingRow) {
+				// Update existing row
+				Object.keys(newRow).forEach((key, index) => {
+					row.getCell(index + 1).value = newRow[key];
+				});
+				row.commit(); // Commit the changes to the row
+			} else {
+				// Add new row if ID is not found
+				worksheet.addRow(newRow);
+			}
+		});
+		await workbook.xlsx.writeFile(filePath);
+	} catch (e) {
+		console.log("Exception:", e);
+	}
+	return filePath;
+});
+
+ipcMain.handle("load-excel", async (event, filePath) => {
+	if (!filePath) filePath = "./assets/archivo.xlsx";
+	console.log("filePath:", filePath);
+	const workbook = new ExcelJS.Workbook();
+	await workbook.xlsx.readFile(filePath);
+	const worksheet = workbook.getWorksheet(1); //Hoja1
+	const data = [];
+	worksheet.eachRow((row, rowNumber) => {
+		if (rowNumber > 1) {
+			const rowData = {};
+			row.eachCell((cell, colNumber) => {
+				rowData[worksheet.getRow(1).getCell(colNumber).value] = cell.value;
+			});
+			data.push(rowData);
+		}
+	});
+
+	return data;
+});
+
+/*
 ipcMain.on("print", (event, content) => {
 	// Read the CSS styles
 	const styles = fs.readFileSync(path.join(__dirname, "printableStyles.css"), "utf-8");
@@ -77,45 +205,9 @@ ipcMain.on("print", (event, content) => {
 	});
 });
 
-ipcMain.handle("save-excel", async (event, data) => {
-	const { filePath } = await dialog.showSaveDialog({
-		title: "Save Excel File",
-		defaultPath: "data.xlsx",
-		filters: [{ name: "Excel Files", extensions: ["xlsx"] }],
-	});
-
-	if (filePath) {
-		const workbook = new ExcelJS.Workbook();
-		const worksheet = workbook.addWorksheet("Hoja 1");
-
-		worksheet.columns = Object.keys(data[0]).map((key) => ({ header: key, key }));
-
-		data.forEach((row) => {
-			worksheet.addRow(row);
-		});
-
-		await workbook.xlsx.writeFile(filePath);
-		return filePath;
-	}
-});
-
-ipcMain.handle("load-excel", async (event, filePath) => {
-	if (!filePath) filePath = "./assets/archivo.xlsx";
-	console.log("filePath:", filePath);
-	const workbook = new ExcelJS.Workbook();
-	await workbook.xlsx.readFile(filePath);
-	const worksheet = workbook.getWorksheet(1); //Hoja1
-	const data = [];
-	worksheet.eachRow((row, rowNumber) => {
-		if (rowNumber > 1) {
-			const rowData = {};
-			row.eachCell((cell, colNumber) => {
-				rowData[worksheet.getRow(1).getCell(colNumber).value] = cell.value;
-			});
-			data.push(rowData);
-			// console.log("RowData:", rowData);
-		}
-	});
-	// console.log("data on main:", data);
-	return data;
-});
+*/
+/*// const { filePath } = await dialog.showSaveDialog({
+	// 	title: "Save Excel File",
+	// 	defaultPath: "data.xlsx",
+	// 	filters: [{ name: "Excel Files", extensions: ["xlsx"] }],
+	// }); */
